909cae0880278b61429924f1abd3fe1daf8afe7a
sa46lll <9812smh@naver.com>
2021-09-12 14:34:13 +0900

    query string

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+    // console.log(_url);
+    
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
         response.writeHead(404);
         response.end();
         return;
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url)); //사용자에게 전송하는 데이터 생성
+    // response.end(queryData.id) //id string 을 출력함.
+    response.end(fs.readFileSync(__dirname + url)); //사용자가 접속한 url에 따라 일치하는 url을 로드해줌.
  
 });
 app.listen(3000);
