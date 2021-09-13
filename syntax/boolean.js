64e5b27be92b12915290639e2960e6273adacd75
sa46lll <9812smh@naver.com>
2021-09-13 23:20:36 +0900

    조건문 기본 문법

---
+++ syntax/boolean.js
@@ -0,0 +1,11 @@
+//boolean
+console.log(true);
+console.log(false);
+
+//comparison
+console.log(1==1); //true
+console.log(1==2); //false
+console.log(1>2); //false
+console.log(1<2); //false
+console.log(1===1); //true 더 정확하냐
+console.log(1===2); //false
