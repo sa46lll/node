9fbbc52b8a23a04d4804afb6945f1febd32ec3ee
sa46lll <9812smh@naver.com>
2021-09-13 23:45:30 +0900

    Not Found 구현

--- main.js
+++ main.js
@@ -5,46 +5,38 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
-    console.log(queryData.id);
-    // console.log(_url);
+    // console.log(url.parse(_url, true));
 
-    if(_url == '/'){ // 홈으로 갔을 때 실행
-      title = 'Welcome';
-      // _url = '/index.html';
-    }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-      var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-    `;
-
-    // response.end(queryData.id) //id string 을 출력함.
-    // response.end(fs.readFileSync(__dirname + url)); //사용자가 접속한 url에 따라 일치하는 url을 로드해줌.
-
-    response.end(template);
-    })
+    if(pathname ==='/'){
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else {
+    response.writeHead(404);
+    response.end('Not Found');
+  }
 
 });
 app.listen(3000);
--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,7 +1,9 @@
+var args = process.argv;
+console.log(args); // 3번째 입력값부터 주도록 고정되어 있음.
 console.log('A');
 console.log('B');
 
-if(false) {
+if(args[2] === '1') {
     console.log('C1');
 } else {
     console.log('C2');
