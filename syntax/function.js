377767fd4396e581eab543a1e59cdac54edbf0ff
sa46lll <9812smh@naver.com>
2021-09-14 21:32:54 +0900

    함수

---
+++ syntax/function.js
@@ -0,0 +1,20 @@
+// function1
+function f123(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
+
+console.log('a');
+console.log('b');
+console.log('c');
+f123();
+
+// function2
+console.log(Math.round(1.6))
+
+function sum(a, b){ //a -> parameter 매개변수
+ return a + b;
+}
+
+console.log(sum(2, 4)); //2 -> argument 인자
