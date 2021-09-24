f623f1ecddc4f7b0a08fc50c61905239e1f0d774
sa46lll <9812smh@naver.com>
2021-09-25 01:28:19 +0900

    입력정보 보안

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
+var path = require('path');
 
 
 var app = http.createServer(function(request,response){
@@ -35,7 +36,8 @@ var app = http.createServer(function(request,response){
         })
       } else {
         fs.readdir('./data', function(arr, filelist){
-          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var filteredId = path.parse(queryData.id).base;
+          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
           var title = queryData.id;
           var list = template.list(filelist);
           var html = template.HTML(title, list,
@@ -90,7 +92,8 @@ var app = http.createServer(function(request,response){
     });
   } else if(pathname === '/update'){
     fs.readdir('./data', function(arr, filelist){
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+      var filteredId = path.parse(queryData.id).base;
+      fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
       var title = queryData.id;
       var list = template.list(filelist);
       var html = template.HTML(title, list,
@@ -142,7 +145,8 @@ var app = http.createServer(function(request,response){
     request.on('end', function(){
       var post = qs.parse(body);
       var id = post.id;
-      fs.unlink(`data/${id}`, function(error){
+      var filteredId = path.parse(id).base;
+      fs.unlink(`data/${filteredId}`, function(error){
         response.writeHead(302, {Location: `/`});
         response.end();
       });
---
+++ password.js
@@ -0,0 +1,4 @@
+module.exports = {
+    id:'sa46lll',
+    password:"111111111"
+}
